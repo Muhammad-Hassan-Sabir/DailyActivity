010f108379322de7b6bbf6944ff22111be316a4a
m.hassan <m.hassan@avanzasolutions.com>
2023-04-18 15:43:25 +0500

    - Thin controllers in the API -Centralized Mediatr in api

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -11,17 +11,10 @@ namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly IMediator mediator;
-
-        public ActivitiesController(IMediator mediator)
-        {
-            this.mediator = mediator;
-        }
-
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> Activities()
         {
-            return await mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
